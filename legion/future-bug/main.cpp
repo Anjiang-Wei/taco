702e4d55637a123594f25d6079591db078b03623
Rohan Yadav <rohany@alumni.cmu.edu>
2021-08-18 10:24:42 -0400

    I got a backpressuring mapper to work!

--- legion/future-bug/main.cpp
+++ legion/future-bug/main.cpp
@@ -1,8 +1,9 @@
 #include "legion.h"
+#include "mappers/default_mapper.h"
 #include <unistd.h>
 
 using namespace Legion;
-
+using namespace Legion::Mapping;
 
 enum FieldIDs {
   FID_VAL,
@@ -13,39 +14,96 @@ enum TaskIDs {
   TID_WORKER,
 };
 
-void workerGPU(const Task* task, const std::vector<PhysicalRegion>& regions, Context ctx, Runtime* runtime) {
+// Let's write a custom mapper to try and buffer the tasks.
+class MyMapper : public Mapping::DefaultMapper {
+public:
+  MyMapper(MapperRuntime* rt, Machine machine, Processor local) : DefaultMapper(rt, machine, local) {}
+  void select_tasks_to_map(const MapperContext ctx,
+                           const SelectMappingInput& input,
+                                 SelectMappingOutput& output) override {
+    MapperEvent returnEvent;
+    // Find the depth of the deepest task
+    int max_depth = 0;
+    for (std::list<const Task*>::const_iterator it =
+        input.ready_tasks.begin(); it != input.ready_tasks.end(); it++)
+    {
+      int depth = (*it)->get_depth();
+      if (depth > max_depth)
+        max_depth = depth;
+    }
+    unsigned count = 0;
+    // Only schedule tasks from the max depth in any pass
+    for (std::list<const Task*>::const_iterator it =
+        input.ready_tasks.begin(); (count < max_schedule_count) &&
+                                   (it != input.ready_tasks.end()); it++)
+    {
+      auto task = *it;
+      bool schedule = true;
+      if (task->task_id == TID_WORKER) {
+        // TODO (rohany): Is target proc valid to do here? What is the ordering
+        //  of mapper calls between select_task_options and this?
+        auto waitEvent = this->queue[task->target_proc];
+        if (waitEvent.exists()) {
+          // If there is an event, then wait behind it.
+          schedule = false;
+          returnEvent = waitEvent;
+        } else {
+          // Otherwise, create an event to listen on.
+          this->queue[task->target_proc] = this->runtime->create_mapper_event(ctx);
+        }
+      }
+      if (schedule && (*it)->get_depth() == max_depth)
+      {
+        output.map_tasks.insert(*it);
+        count++;
+      }
+    }
+    if (output.map_tasks.size() == 0) {
+      assert(returnEvent.exists());
+      output.deferral_event = returnEvent;
+    }
+  }
+
+  void map_task(const MapperContext ctx,
+                const Task& task,
+                const MapTaskInput& input,
+                MapTaskOutput& output) override {
+    DefaultMapper::map_task(ctx, task, input, output);
+    if (task.task_id == TID_WORKER) {
+      output.task_prof_requests.add_measurement<ProfilingMeasurements::OperationStatus>();
+    }
+  }
+
+  void report_profiling(const MapperContext ctx,
+                        const Task& task,
+                        const TaskProfilingInfo& input) override {
+    assert(task.task_id == TID_WORKER);
+    auto prof = input.profiling_responses.get_measurement<ProfilingMeasurements::OperationStatus>();
+    assert(prof->result == Realm::ProfilingMeasurements::OperationStatus::COMPLETED_SUCCESSFULLY);
+    auto event = this->queue[task.target_proc];
+    assert(event.exists());
+    this->queue[task.target_proc] = MapperEvent();
+    this->runtime->trigger_mapper_event(ctx, event);
+  }
+
+  MapperSyncModel get_mapper_sync_model() const override {
+    // TODO (rohany): If I wanted to loosen the mapper serialization model here, where would I
+    // need locks? Probably around the queue itself? Might also be easier to just
+    // take a lock on entry to these overridden functions.
+    return SERIALIZED_NON_REENTRANT_MAPPER_MODEL;
+  }
+
+  // Map from processor to MapperEvents.
+  std::map<Processor, MapperEvent> queue;
+};
+
+void worker(const Task* task, const std::vector<PhysicalRegion>& regions, Context ctx, Runtime* runtime) {
   auto workerIdx = *(int32_t*)task->args;
   std::cout << "starting worker " << workerIdx << std::endl;
-
-  // Now let's allocate a buffer to yield control back to the runtime.
   double initVal = 0.f;
-  // Allocate and fill 4 GB of data.
-  DeferredBuffer<double, 1> buf(Memory::Kind::GPU_FB_MEM, DomainT<1>(Rect<1>(0, 50000000)), &initVal);
-  // DeferredBuffer<double, 1> buf(Memory::Kind::GPU_FB_MEM, DomainT<1>(Rect<1>(0, 50000000)));
-  double* inter = buf.ptr(0);
-
-  usleep(500 * 1000);
-  
-  std::cout << "finishing worker " << workerIdx << std::endl;
-  // return 0;
-}
-
-void workerCPU(const Task* task, const std::vector<PhysicalRegion>& regions, Context ctx, Runtime* runtime) {
-  auto workerIdx = *(int32_t*)task->args;
-  std::cout << "starting worker " << workerIdx << std::endl;
-
-  // Now let's allocate a buffer to yield control back to the runtime.
-  double initVal = 0.f;
-  // Allocate and fill 4 GB of data.
   DeferredBuffer<double, 1> buf(Memory::Kind::SYSTEM_MEM, DomainT<1>(Rect<1>(0, 500000)), &initVal);
-  // DeferredBuffer<double, 1> buf(Memory::Kind::GPU_FB_MEM, DomainT<1>(Rect<1>(0, 50000000)), &initVal);
-  // DeferredBuffer<double, 1> buf(Memory::Kind::GPU_FB_MEM, DomainT<1>(Rect<1>(0, 50000000)));
-  double* inter = buf.ptr(0);
-
   usleep(500 * 1000);
-  
   std::cout << "finishing worker " << workerIdx << std::endl;
-  // return 0;
 }
 
 void top_level_task(const Task* task, const std::vector<PhysicalRegion>& regions, Context ctx, Runtime* runtime) {
@@ -68,6 +126,11 @@ void top_level_task(const Task* task, const std::vector<PhysicalRegion>& regions
   }
 }
 
+void register_mapper(Machine machine, Runtime* runtime, const std::set<Processor> &local_procs) {
+  MyMapper* m = new MyMapper(runtime->get_mapper_runtime(), machine, *local_procs.begin());
+  runtime->replace_default_mapper(m);
+}
+
 int main(int argc, char** argv) {
   Runtime::set_top_level_task_id(TID_TOP_LEVEL);
   {
@@ -78,17 +141,10 @@ int main(int argc, char** argv) {
   }
   {
     TaskVariantRegistrar registrar(TID_WORKER, "worker");
-    registrar.add_constraint(ProcessorConstraint(Processor::TOC_PROC));
-    registrar.set_leaf();
-    // Runtime::preregister_task_variant<int,worker>(registrar, "worker");
-    Runtime::preregister_task_variant<worker>(registrar, "worker");
-  }
-  {
-    TaskVariantRegistrar registrar(TID_WORKER, "worker");
     registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
     registrar.set_leaf();
-    // Runtime::preregister_task_variant<int,worker>(registrar, "worker");
     Runtime::preregister_task_variant<worker>(registrar, "worker");
   }
+  Runtime::add_registration_callback(register_mapper);
   return Runtime::start(argc, argv);
 }
