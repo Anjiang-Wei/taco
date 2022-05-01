69f2573099f929f64d2079ea81cee6177e8a3d55
Rohan Yadav <rohany@alumni.cmu.edu>
2022-05-01 11:45:16 -0700

    test,legion/spmm: add batched and memory conserving schedules
    This commit adds in some different schedules for SpMM but doesn't
    integrate them into the driver code yet.

--- legion/spmm/taco-generated.h
+++ legion/spmm/taco-generated.h
@@ -8,10 +8,32 @@ struct partitionPackForcomputeLegion {
   LegionTensorPartition BPartition;
 };
 
+struct partitionPackForcomputeLegionMemCons {
+  LegionTensorPartition APartition;
+  LegionTensorPartition BPartition;
+  LegionTensorPartition CPartition;
+};
+
+struct partitionPackForcomputeLegionBatched {
+  LegionTensorPartition APartition;
+  LegionTensorPartition BPartition;
+  LegionTensorPartition CPartition;
+};
+
 
 partitionPackForcomputeLegion partitionForcomputeLegion(Legion::Context ctx, Legion::Runtime* runtime, LegionTensor* A, LegionTensor* B, LegionTensor* C, int32_t gx);
 
 
 void computeLegion(Legion::Context ctx, Legion::Runtime* runtime, LegionTensor* A, LegionTensor* B, LegionTensor* C, partitionPackForcomputeLegion* partitionPack, int32_t gx);
+
+partitionPackForcomputeLegionMemCons partitionForcomputeLegionMemCons(Legion::Context ctx, Legion::Runtime* runtime, LegionTensor* A, LegionTensor* B, LegionTensor* C, int32_t gx, int32_t gy);
+
+
+void computeLegionMemCons(Legion::Context ctx, Legion::Runtime* runtime, LegionTensor* A, LegionTensor* B, LegionTensor* C, partitionPackForcomputeLegionMemCons* partitionPack, int32_t gx, int32_t gy);
+
+partitionPackForcomputeLegionBatched partitionForcomputeLegionBatched(Legion::Context ctx, Legion::Runtime* runtime, LegionTensor* A, LegionTensor* B, LegionTensor* C, int32_t gy, int32_t gx);
+
+
+void computeLegionBatched(Legion::Context ctx, Legion::Runtime* runtime, LegionTensor* A, LegionTensor* B, LegionTensor* C, partitionPackForcomputeLegionBatched* partitionPack, int32_t gy, int32_t gx);
 void registerTacoTasks();
 #endif // TACO_GENERATED_H
