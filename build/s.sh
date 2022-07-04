df359e88847a1ec2a10783f8c36e1e71c5f557af
Anjiang-Wei <1020681930@qq.com>
2022-07-04 16:17:39 -0700

    new run works

--- build/s.sh
+++ build/s.sh
@@ -7,4 +7,5 @@
 # mpirun ./circuit -mapping mappings_circuit -level nsmapper=debug -ll:gpu 4 -ll:cpu 2
 
 # mpirun ./bin/cannonMM -n 8192 -gx 2 -gy 1 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0
-mpirun ./bin/cannonMM -n 1024 -gx 2 -gy 1 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0
+# mpirun ./bin/cannonMM -n 1024 -gx 2 -gy 1 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0
+mpirun -bind-to none ./bin/cannonMM -n 8192 -gx 2 -gy 1 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0
