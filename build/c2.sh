56593bd30a3c49e26ab927561a7016a8ecf2ebd5
Anjiang-Wei <1020681930@qq.com>
2022-07-26 16:31:28 -0700

    misunderstand rebuilding legion

--- build/c2.sh
+++ build/c2.sh
@@ -1,2 +1,3 @@
+# cd ../deps-install/legion-build
 cmake ../ -DCMAKE_BUILD_TYPE=Debug -DLegion_BOUNDS_CHECKS=ON -DLegion_PRIVILEGE_CHECKS=ON -DLegion_SPY=ON
-USE_SPY=1 make -j taco-test && USE_SPY=1 bin/taco-test --gtest_filter="distributed.cuda_cannonMM" && USE_SPY=1 make -j cannonMM-cuda
+make -j taco-test && bin/taco-test --gtest_filter="distributed.cuda_cannonMM" && make -j cannonMM-cuda
