a569b46bb1bc96be3bf6644a2344dedb44440db9
Anjiang-Wei <1020681930@qq.com>
2022-07-28 21:33:42 -0700

    refactor slurm

--- build/s2.sh
+++ build/s2.sh
@@ -11,12 +11,15 @@
 # -lg:spy -logfile spy_%.log
 # -lg:safe_ctrlrepl -lg:warn
 #  -dm:replicate 0
-# -level mapper=debug -logfile mapper_taco%.log -lg:prof 2 -lg:prof_logfile prof_%.gz
-# -level mapper=debug -logfile mapper_dsl%.log -lg:prof 2 -lg:prof_logfile prof_dsl%.gz
+# -level mapper=debug -logfile mapper_taco%.log
+# -lg:prof 2 -lg:prof_logfile prof_taco%.gz
+# 
+# -level mapper=debug -logfile mapper_dsl%.log
+# -lg:prof 2 -lg:prof_logfile prof_dsl%.gz
 # -level nsmapper=debug
 
 # mpirun -H g0002,g0003 --bind-to none ./bin/cannonMM -n 8192 -gx 2 -gy 2 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0 -mapping mappings -level nsmapper=debug
 # mpirun --bind-to none ./bin/cannonMM-cuda -n 8192 -lg:partcheck -lg:safe_mapper -ll:gpu 4 -ll:fsize 15G -ll:ocpu 1 -ll:othr 1 -ll:nsize 30G -ll:ncsize 0 -gx 1 -gy 2 -mapping mappings -level nsmapper=debug -level mapper=debug # for taco_mapper
 
 # mpirun --bind-to none ./bin/cannonMM-cuda -n 8192 -ll:gpu 4 -ll:fsize 15G -ll:ocpu 1 -ll:othr 1 -ll:nsize 30G -ll:ncsize 0 -gx 1 -gy 2 -mapping mappings 
-mpirun --bind-to none ./bin/cannonMM-cuda -n 8192 -ll:gpu 4 -ll:fsize 15G -ll:ocpu 1 -ll:othr 1 -ll:nsize 30G -ll:ncsize 0 -gx 1 -gy 2 -mapping mappings -level mapper=debug -logfile mapper_dsl%.log -lg:prof 2 -lg:prof_logfile prof_dsl%.gz
+mpirun --bind-to none ./bin/cannonMM-cuda -n 20000 -ll:gpu 4 -ll:fsize 15G -ll:ocpu 1 -ll:othr 1 -ll:nsize 30G -ll:ncsize 0 -gx 1 -gy 2 -mapping mappings -level nsmapper=debug
