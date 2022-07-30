9157ba3c1d2dde4c4d84fc7e6c711bf0045f8b6e
Anjiang-Wei <1020681930@qq.com>
2022-07-29 21:05:10 -0700

    remove useless logs

--- build/c2.sh
+++ build/c2.sh
@@ -1,4 +1,5 @@
 rm ./bin/cannonMM-cuda
 # cd ../deps-install/legion-build
-cmake ../ -DCMAKE_BUILD_TYPE=Debug
-make -j20 taco-test && bin/taco-test --gtest_filter="distributed.cuda_cannonMM" && make -j20 cannonMM-cuda
+# cmake ../ -DCMAKE_BUILD_TYPE=Debug
+# make -j20 taco-test && bin/taco-test --gtest_filter="distributed.cuda_cannonMM" &&
+make -j20 cannonMM-cuda
