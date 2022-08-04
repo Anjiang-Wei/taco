29df45a8b7cfa9cf7243ceb3d6849cd778094e0a
Anjiang-Wei <1020681930@qq.com>
2022-08-04 16:50:09 -0700

    upload scripts for summa

---
+++ build/summaMM/s2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH -n 2
+#SBATCH -c 40
+#SBATCH -p gpu
+
+mpirun --bind-to none ../bin/summaMM-cuda -n 20000 -ll:gpu 4 -ll:fsize 15G -ll:ocpu 1 -ll:othr 1 -ll:nsize 30G -ll:ncsize 0 -gx 1 -gy 2
+mpirun --bind-to none ../bin/summaMM-cuda -n 20000 -ll:gpu 4 -ll:fsize 15G -ll:ocpu 1 -ll:othr 1 -ll:nsize 30G -ll:ncsize 0 -gx 1 -gy 2 -mapping mappings -dslmapper
