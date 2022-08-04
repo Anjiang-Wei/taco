1dd570529492f07f68aaa58a8f0122d6b2f8af8a
Anjiang-Wei <1020681930@qq.com>
2022-08-04 14:22:16 -0700

    pumma running scripts

---
+++ build/pummaMM/s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH -n 2
+#SBATCH -c 40
+#SBATCH -p gpu
+
+mpirun --bind-to none ../bin/pummaMM -n 8192 -gx 2 -gy 2 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0
+mpirun --bind-to none ../bin/pummaMM -n 8192 -gx 2 -gy 2 -ll:ocpu 2 -ll:gpu 0 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0 -dslmapper -mapping mappings_cpu
