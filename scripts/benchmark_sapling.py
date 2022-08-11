5507754b017d6948d7b7bfcb0963335491dcf98d
Anjiang-Wei <1020681930@qq.com>
2022-08-11 15:13:28 -0700

    johnson's works

--- scripts/benchmark_sapling.py
+++ scripts/benchmark_sapling.py
@@ -54,16 +54,27 @@ def lgGPUArgs(gpus):
 
 def lgGPUMultShardsArgs(gpus):
     return [
-      '-ll:cpu', '4',
-      '-ll:csize', '150000',
-      '-ll:util', '4',
-      '-dm:replicate', '1',
-      '-ll:gpu', str(gpus),
-      '-ll:fsize', '15000',
-      '-ll:bgwork', '12',
-      '-ll:bgnumapin', '1',
+      '-ll:ocpu', '1',
+      '-ll:othr', '1',
+      '-ll:gpu', '4',
+      '-ll:fsize', '15G',
+      '-ll:nsize', '30G',
+      '-ll:ncsize', '0',
       '-tm:multiple_shards_per_node',
+      '-level', 'mapper=debug',
+      '-logfile mapper44_taco%.log',
     ]
+    # return [
+    #   '-ll:cpu', '4',
+    #   '-ll:csize', '150000',
+    #   '-ll:util', '4',
+    #   '-dm:replicate', '1',
+    #   '-ll:gpu', str(gpus),
+    #   '-ll:fsize', '15000',
+    #   '-ll:bgwork', '12',
+    #   '-ll:bgnumapin', '1',
+    #   '-tm:multiple_shards_per_node',
+    # ]
 
 
 def lassenHeader(procs):
