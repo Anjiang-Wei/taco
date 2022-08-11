6c46356fee50c2eb0262339243b9c39dda6e55d1
Anjiang-Wei <1020681930@qq.com>
2022-08-11 16:24:18 -0700

    solomonik and summa finish

---
+++ build/solomonikMM-cuda/s4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 4
+#SBATCH -n 4
+#SBATCH -c 40
+#SBATCH -p gpu
+
+mpirun -n 4 -npernode 1 --bind-to none ../bin/solomonikMM-cuda -n 20000 -rpoc 4 -c 1 -rpoc3 4 -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -logfile mapper44_taco%.log
+
+mpirun -n 4 -npernode 1 --bind-to none ../bin/solomonikMM-cuda -n 20000 -rpoc 4 -c 1 -rpoc3 4 -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -level nsmapper=debug -dslmapping -mapping mappings -logfile mapper44_dsl%.log
