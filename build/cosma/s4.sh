ccca73ce3d325bc35367688ab2c9e553507b1e37
Anjiang-Wei <1020681930@qq.com>
2022-08-14 21:05:13 -0700

    cosma works

---
+++ build/cosma/s4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 4
+#SBATCH -n 4
+#SBATCH -c 40
+#SBATCH -p gpu
+
+mpirun -n 4 -npernode 1 --bind-to none ../bin/cosma -n 8192 -gx 2 -gy 2 -gz 2 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0 -level mapper=debug -logfile mapper44_taco%.log
+
+mpirun -n 4 -npernode 1 --bind-to none ../bin/cosma -n 8192 -gx 2 -gy 2 -gz 2 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0 -level mapper=debug -level nsmapper=debug -dslmapper -mapping mappings -logfile mapper44_dsl%.log
