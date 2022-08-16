69332f0761485115cab2318b9a51a7ae1746da4d
Anjiang-Wei <1020681930@qq.com>
2022-08-15 22:15:35 -0700

    ttmc works

---
+++ build/ttmc/s4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 4
+#SBATCH -n 4
+#SBATCH -c 40
+#SBATCH -p gpu
+
+mpirun -n 4 -npernode 1 --bind-to none ../bin/ttmc -n 800 -pieces 8 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0 -level mapper=debug -logfile mapper44_taco%.log
+
+mpirun -n 4 -npernode 1 --bind-to none ../bin/ttmc -n 800 -pieces 8 -ll:ocpu 2 -ll:othr 9 -ll:nsize 3G -ll:ncsize 0 -level mapper=debug -level nsmapper=debug -dslmapper -mapping mappings -logfile mapper44_dsl%.log
