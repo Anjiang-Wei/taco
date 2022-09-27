ad4b4fff3048b4ec4ae3b61fb92d277cb57da8d4
Anjiang-Wei <1020681930@qq.com>
2022-09-26 20:43:48 -0700

    remove _

--- legion/compiler/ptr.py
+++ legion/compiler/ptr.py
@@ -4,7 +4,7 @@ def replace(lines, pre, post):
     res = []
     for line in lines:
         if "return this;" in line:
-            res.append(line.replace("return this;", "return get_ptr();"))
+            res.append(line.replace("return this;", "return getptr();"))
             continue
         if line.startswith(pre):
             res.append(line.replace(pre, post))
