28a9e25244c51fd7d384cd34c13a0aefd47cf395
Anjiang-Wei <1020681930@qq.com>
2022-10-31 21:25:13 -0700

    upload parsing scripts for TACO

--- build/parse_results.py
+++ build/parse_results.py
@@ -7,65 +7,47 @@ import glob
 import os
 import re
 
-# _filename_re = re.compile(r'out_([0-9]+)x([0-9]+)_r([0-9]+)[.]log')
-_filename_re = re.compile(r'([0-9]+)[.]out')
-def parse_basename(filename):
-    match = re.match(_filename_re, filename)
-    assert match is not None
-    return match.groups()
-
-# _content_re = re.compile(r'^ELAPSED TIME = +([0-9.]+) s$', re.MULTILINE)
 # On 16 nodes achieved GFLOPS per node: 8232.518652.
 _content_re = re.compile(r'On +([0-9]+) nodes achieved GFLOPS per node: +([0-9.]+).', re.MULTILINE)
-def parse_content(path, choice):
-    # first appearance, choice = 0 --> TacoMapper
-    # second appearance, choice = 1 --> DSL Mapper
-    res = None
-    match_times = 0
-    assert choice == 0 or choice == 1
+def parse_content(path):
+    # only keep the last two performance results, in this order: Taco, DSL
+    res = []
     with open(path, 'r') as f:
         content = f.readlines()
         for line in content:
             match = re.search(_content_re, line)
             if match is not None:
-                if match_times % 2 == choice:
-                    res = match.groups()
-                match_times += 1
-        return res
-
-# GFLOPS = 485417.560 GFLOPS
-# un-used here
-_content_re2 = re.compile(r'^GFLOPS = +([0-9.]+) GFLOPS$', re.MULTILINE)
-def parse_content2(path):
-    with open(path, 'r') as f:
-        content = f.read()
-        match = re.search(_content_re2, content)
-        if match is None:
-            return ('ERROR',)
-        return match.groups()
+                res.append(match.groups())
+    print(f"{path} has {len(res)} matches")
+    if len(res) < 2:
+        print(f"{path} error, assuming DSL fails")
+        return res[-1], None
+    return res[-2], res[-1]
 
 def main():
     paths = glob.glob('*.out')
     content = []
-    content2 = []
     for path in paths:
         filename = os.path.basename(path)
-        file_c = parse_content(path, 0)
-        if file_c is not None:
-            content.append((filename, *file_c, "TACO"))
-        file_c2 = parse_content(path, 1)
-        if file_c2 is not None:
-            content2.append((filename, *file_c2, "DSL"))
-    content.sort(key=lambda row: (int(row[1]), row[0]))
-    content2.sort(key=lambda row: (int(row[1]), row[0]))
-
+        taco_perf, dsl_perf = parse_content(path)
+        # 0:filename, 1:nodes, 2:taco_gflops, 3:dsl_gflops
+        res = [filename, "Error", "Error", "Error"]
+        if taco_perf is not None:
+            res[1] = int(taco_perf[0]) # node count
+            res[2] = float(taco_perf[1]) # gflops
+        if dsl_perf is not None:
+            if res[1] == int(dsl_perf[0]): # node count mismatch
+                res[3] = float(dsl_perf[1]) # gflops
+            else:
+                print(f"Error in {filename}")
+        content.append(res)
+    content.sort(key=lambda row: (row[1], row[0]))
 
     import sys
     # with open(out_filename, 'w') as f:
     out = csv.writer(sys.stdout)# , dialect='excel-tab') # f)
-    out.writerow(['filename', 'nodes', 'gflops', 'tag'])
+    out.writerow(['filename', 'nodes', 'taco_gflops', 'dsl_gflops'])
     out.writerows(content)
-    out.writerows(content2)
 
 if __name__ == '__main__':
     main()
