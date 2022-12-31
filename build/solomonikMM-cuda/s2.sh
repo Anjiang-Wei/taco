1332ec0bf61661744a2e7fb50a7e71dac644bc85
Anjiang-Wei <1020681930@qq.com>
2022-12-31 10:20:02 -0800

    new mapping syntax without GPU in backpressure

---
+++ build/solomonikMM-cuda/s2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH -n 2
+#SBATCH -c 40
+#SBATCH -p gpu
+
+mpirun -n 2 -npernode 1 --bind-to none ../bin/solomonikMM-cuda -n 1000 -rpoc 2 -c 2 -rpoc3 2 -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -logfile mapper44_taco%.log
+
+mpirun -n 2 -npernode 1 --bind-to none ../bin/solomonikMM-cuda -n 1000 -rpoc 2 -c 2 -rpoc3 2 -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -level nsmapper=debug -dslmapper -mapping mappings -logfile mapper44_dsl%.log
