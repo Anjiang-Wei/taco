b3080a7f0554b7ad0ec8abeb232febd2b92c4bbd
Anjiang-Wei <1020681930@qq.com>
2023-01-05 23:55:24 -0800

    update s2 and s4 and cannonMM-cuda mapping

--- build/cannonMM-cuda/s4.sh
+++ build/cannonMM-cuda/s4.sh
@@ -4,6 +4,6 @@
 #SBATCH -c 40
 #SBATCH -p gpu
 
-mpirun -n 4 -npernode 1 --bind-to none ../bin/cannonMM-cuda -n 20000 -gx 2 -gy 2 -dm:exact_region -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -logfile mapper44_taco%.log
+mpirun -n 4 -npernode 1 --bind-to none ../bin/cannonMM-cuda -n 20000 -gx 2 -gy 2 -dm:exact_region -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -logfile mapper44_taco%.log -wrapper
 
-mpirun -n 4 -npernode 1 --bind-to none ../bin/cannonMM-cuda -n 20000 -gx 2 -gy 2 -dm:exact_region -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -level nsmapper=debug -dslmapper -mapping mappings -logfile mapper44_dsl%.log
+mpirun -n 4 -npernode 1 --bind-to none ../bin/cannonMM-cuda -n 20000 -gx 2 -gy 2 -dm:exact_region -tm:untrack_valid_regions -ll:ocpu 1 -ll:othr 1 -ll:gpu 4 -ll:fsize 15G -ll:nsize 30G -ll:ncsize 0 -level mapper=debug -level nsmapper=debug -dslmapper -mapping mappings -logfile mapper44_dsl%.log -wrapper
