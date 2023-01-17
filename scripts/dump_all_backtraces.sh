8f3356f7ea1684e02308fd1a94ba8d83f87717a2
Anjiang-Wei <1020681930@qq.com>
2023-01-17 12:32:48 -0800

    fix a bug

--- scripts/dump_all_backtraces.sh
+++ scripts/dump_all_backtraces.sh
@@ -5,6 +5,7 @@ set -x
 root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 job="$1"
+# job is the path to the job file (*.out)
 scratch_dir="$2"
 mkdir -p "$scratch_dir"
 
@@ -12,7 +13,7 @@ i=0
 if command -v bjobs &> /dev/null
 then
     # hosts="$(bjobs -o exec_host $job | tail -n -1 | tr ':' '\n' | sort -u | tr '\n' ' ')"
-    hosts="$(grep "All hosts" ${job}.out | tr ' ' '\n' | tail -n +4 | sort -u | tr '\n' ' ')"
+    hosts="$(grep "All hosts" ${job} | tr ' ' '\n' | tail -n +4 | sort -u | tr '\n' ' ')"
 else
     hosts="$(sacct --json -j $job | jq -r '.jobs[0]|.steps[1]|.nodes|.list|@sh' | tr -d \')"
 
