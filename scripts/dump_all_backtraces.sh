5f7c171fbaa189dfd73ddaba22465e2e4f5ec5f3
Anjiang-Wei <1020681930@qq.com>
2023-01-17 12:29:31 -0800

    add dump trace scripts

---
+++ scripts/dump_all_backtraces.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+job="$1"
+scratch_dir="$2"
+mkdir -p "$scratch_dir"
+
+i=0
+if command -v bjobs &> /dev/null
+then
+    # hosts="$(bjobs -o exec_host $job | tail -n -1 | tr ':' '\n' | sort -u | tr '\n' ' ')"
+    hosts="$(grep "All hosts" ${job}.out | tr ' ' '\n' | tail -n +4 | sort -u | tr '\n' ' ')"
+else
+    hosts="$(sacct --json -j $job | jq -r '.jobs[0]|.steps[1]|.nodes|.list|@sh' | tr -d \')"
+
+fi
+for host in $hosts; do
+    ssh $host bash "$root_dir/dump_node_backtraces.sh" "$scratch_dir" &
+    if [[ $(( i % 200 )) == 0 ]]; then
+        wait
+    fi
+    let i++
+done
+
+wait
+
---
+++ scripts/dump_node_backtraces.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+out_dir="$1"
+BINARY="cannonMM-cuda"
+pstree -u $(whoami) -p > "$out_dir/pstree_$(hostname).log"
+for pid in $(pgrep -u $(whoami) ${BINARY}); do
+    gdb -p $pid -batch -quiet -ex "thread apply all bt" 2>&1 > "$out_dir/bt_$(hostname)_$pid.log"
+    if grep realm_freeze "$out_dir/bt_$(hostname)_$pid.log"; then
+        gdb -p $pid -batch -quiet -ex "gcore $(hostname)_$pid.core"
+    fi
+done
