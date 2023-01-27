e333b0bfd3cd5ac00db3a2a0cce12ebcc2116a09
Anjiang-Wei <1020681930@qq.com>
2023-01-27 15:53:41 -0800

    fix the bug

--- scripts/dump_node_backtraces.sh
+++ scripts/dump_node_backtraces.sh
@@ -4,7 +4,8 @@ BINARY="$1"
 out_dir="$2"
 
 pstree -u $(whoami) -p > "$out_dir/pstree_$(hostname).log"
-for pid in $(pgrep -u $(whoami) ${BINARY}); do
+for pid in $(ps -aux | grep $(whoami) | grep ${BINARY} | grep -v grep  | tr -s " " | cut -d " " -f 2); do
+# for pid in $(pgrep -u $(whoami) ${BINARY}); do
     gdb -p $pid -batch -quiet -ex "thread apply all bt" 2>&1 > "$out_dir/bt_$(hostname)_$pid.log"
     if grep realm_freeze "$out_dir/bt_$(hostname)_$pid.log"; then
         gdb -p $pid -batch -quiet -ex "gcore $(hostname)_$pid.core"
