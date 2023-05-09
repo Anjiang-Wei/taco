a01d8be45b4b2aaf866740d66ca4c4fa4603f800
Anjiang-Wei <1020681930@qq.com>
2023-05-09 10:40:24 -0700

    use CMAKE_CXX_FLAGS

--- scripts/debug_hang.py
+++ scripts/debug_hang.py
@@ -45,9 +45,7 @@ def cmake(dir, defines={}, env=None):
     cmakeCmd = ["cmake", dir]
     for key, value in defines.items():
         assert isinstance(key, str)
-        if key == "LEGION_SEPARATE_META_TASKS":
-            cmakeCmd.append("-DLEGION_SEPARATE_META_TASKS=ON")
-        elif isinstance(value, bool):
+        if isinstance(value, bool):
             cmakeCmd.append("-D{0}={1}".format(key, "ON" if value else "OFF"))
         elif isinstance(value, str) or isinstance(value, int):
             cmakeCmd.append("-D{0}={1}".format(key, value))
@@ -136,11 +134,11 @@ with pushd(args.deps_install_dir):
         # shared library with the configured RPATH into DISTAL.
         cmakeDefs = {
             "BUILD_SHARED_LIBS": True,
-            "CMAKE_CXX_FLAGS": "--std=c++11",
+            "CMAKE_CXX_FLAGS": "--std=c++11 -DLEGION_SEPARATE_META_TASKS",
             "CMAKE_BUILD_TYPE": "Release",
             "CMAKE_INSTALL_PREFIX": cmakeInstallPath,
             "Legion_USE_HDF5": True,
-            "LEGION_SEPARATE_META_TASKS": "should_be_-DLEGION_SEPARATE_META_TASKS"
+            # "LEGION_SEPARATE_META_TASKS": "should_be_-DLEGION_SEPARATE_META_TASKS"
             # "Legion_SPY": True, # below 3 specificially added for debugging
             # "Legion_BOUNDS_CHECKS": True,
             # "Legion_PRIVILEGE_CHECKS": True,
@@ -173,7 +171,6 @@ with pushd(args.distal_build_dir):
         "CMAKE_BUILD_TYPE": "Release",
         "CMAKE_MODULE_PATH": os.path.join(distalRoot, "cmake"),
         "CMAKE_PREFIX_PATH": ";".join([cmakeInstallPath, makeInstallPath]),
-        "LEGION_SEPARATE_META_TASKS": "also -DLEGION_SEPARATE_META_TASKS=ON"
     }
     if args.openmp:
         cmakeDefs["OPENMP"] = True
